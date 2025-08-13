68075663a477a79c3d6fd49438caac384b0b856b
binarrii <dupp@wasu.com>
2025-08-13 17:24:37 +0800

    init

--- env.py
+++ env.py
@@ -4,7 +4,7 @@ import dotenv
 
 dotenv.load_dotenv(dotenv_path=f"{os.path.dirname(__file__)}/.env")
 
-VL_MODEL = os.getenv("VL_MODEL", "Qwen/Qwen2.5-VL-7B-Instruct-AWQ")
+VL_MODEL = os.getenv("VL_MODEL", "Qwen/Qwen2.5-VL-7B-Instruct")
 VL_MODEL_BASE_URL = os.getenv("VL_MODEL_BASE_URL", "").rstrip("/")
 VL_MODEL_API_KEY = os.getenv("VL_MODEL_API_KEY", "")
 
