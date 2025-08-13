1f6f0c8ae5a9edcecefdf08f2b9bf1348ce5ebc8
binarrii <dupp@wasu.com>
2025-08-13 17:26:03 +0800

    init

--- env.py
+++ env.py
@@ -8,4 +8,4 @@ VL_MODEL = os.getenv("VL_MODEL", "Qwen/Qwen2.5-VL-7B-Instruct")
 VL_MODEL_BASE_URL = os.getenv("VL_MODEL_BASE_URL", "").rstrip("/")
 VL_MODEL_API_KEY = os.getenv("VL_MODEL_API_KEY", "")
 
-OUTPUT_HTTP_PREFIX="http://10.252.25.251:8186"
+OUTPUT_HTTP_PREFIX=os.getenv("OUTPUT_HTTP_PREFIX", "")
